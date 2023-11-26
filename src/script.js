f011943bfa8ef5510f08c78592df623e4a46fbfc
Tiirnako <treeleaves229@gmail.com>
2023-11-26 02:34:01 -0600

    Add menus
    Main menu added
    selectable player marker and name

--- src/script.js
+++ src/script.js
@@ -1,15 +1,44 @@
-function createPlayer(name, marker) {
-    const playerName = name;
-    const playerMarker = marker;
+function gameStart() {
+    const playerName = document.getElementById('name').value;
+    const marker = document.querySelector('input[name="marker"]:checked').value;
+    const gameGrid = document.querySelector('.grid');
+    const playerSelect = document.querySelector('.playerSelect');
 
-    return { playerName, playerMarker };
+    playerSelect.classList = 'playerSelect hidden';
+    gameGrid.classList = 'grid';
+
+    gameboard(playerName, marker);
 }
 
-function gameboard() {
+function beginGame() {
+    
+    const mainMenu = document.querySelector('.mainMenu');
+    const playerSelect = document.querySelector('.playerSelect');
+    
+    mainMenu.classList = 'mainMenu hidden';
+    playerSelect.classList = 'playerSelect';
+
+}
+
+function gameboard(playerName, playerSelectedMarker) {
+ 
+    function createPlayer(name, marker) {
+        const playerName = name;
+        const playerMarker = marker;
+    
+        return { playerName, playerMarker };
+    }
+
     const board = ['','','','','','','','','']
     const emptySlots = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
-    const player = createPlayer("Steve", 'X');
-    const opponent = createPlayer("Arnold", "O");
+    const player = createPlayer(playerName, playerSelectedMarker);
+    let opponent;
+    
+    if (player.playerMarker == "X") {
+        opponent = createPlayer("Arnold", "O");
+    } else {
+        opponent = createPlayer("Arnold", "X");
+    }
 
     for (i = 0; i < board.length; i++) {
         const index = i;
@@ -18,37 +47,37 @@ function gameboard() {
             addPlayerMarker(player.playerMarker, opponent.playerMarker, board, index, emptySlots);             
         })
     }
-}
-
-function randomNum(emptySlots) {
-    const rand = Math.floor(Math.random() * emptySlots.length);
-    return rand;
-}
-
-function addOpponentMarker(opponentMarker, board, emptySlots) {
-    const randNum = randomNum(emptySlots);
-    const opponentIndex = Number(emptySlots[randNum]);
-    board[opponentIndex] = opponentMarker;
-    emptySlots.splice(randNum, 1);
+    
 }
 
 function addPlayerMarker(playerMarker, opponentMarker, board, index, emptySlots) {
+    
+    function addOpponentMarker(opponentMarker, board, emptySlots) {
+        function randomNum(emptySlots) {
+            const rand = Math.floor(Math.random() * emptySlots.length);
+            return rand;
+        }
+        const randNum = randomNum(emptySlots);
+        const opponentIndex = Number(emptySlots[randNum]);
+        board[opponentIndex] = opponentMarker;
+        emptySlots.splice(randNum, 1);
+    }
+
+    function displayGameBoard(board) {
+        for (i = 0; i < board.length; i++) {
+            let gridSlot = document.getElementById(i);
+            gridSlot.innerHTML = board[i];
+        }
+    }
+    
     if(!board[index]){
         const boardIndex = index;
-        board[boardIndex] = playerMarker
+        board[boardIndex] = playerMarker;
         emptySlots.splice(emptySlots.indexOf(boardIndex.toString()), 1);
-        addOpponentMarker(opponentMarker, board, emptySlots)
+        addOpponentMarker(opponentMarker, board, emptySlots);
         displayGameBoard(board);    
         } else if(board[index] == opponentMarker || board[index] == playerMarker) {
-            console.log('try a new spot')
+            console.log('try a new spot');
     } 
 }
 
-function displayGameBoard(board) {
-    for (i = 0; i < board.length; i++) {
-        let gridSlot = document.getElementById(i);
-        gridSlot.innerHTML = board[i]
-    }
-}
-
-gameboard();
