d66915e8393818f81b363f302cf7b2fb54f8ddc5
Tiirnako <treeleaves229@gmail.com>
2023-11-25 22:56:56 -0600

    Add tictactoe function
    add function to play the game and display to the DOM

--- src/script.js
+++ src/script.js
@@ -7,42 +7,48 @@ function createPlayer(name, marker) {
 
 function gameboard() {
     const board = ['','','','','','','','','']
+    const emptySlots = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
     const player = createPlayer("Steve", 'X');
     const opponent = createPlayer("Arnold", "O");
 
     for (i = 0; i < board.length; i++) {
         const index = i;
-        document.getElementById(i).addEventListener('click', () => {
-            console.log(index);
-            addPlayerMarker(player.playerMarker, opponent.playerMarker, board, index);
-            addOpponentMarker(opponent.playerMarker, player.playerMarker, board, index);
-        }
-
-        )
-    }
-    
-    return {
-        board,
-        player,
-        opponent,        
+        const boardBox = document.getElementById(i);
+        boardBox.addEventListener('click', () => {
+            addPlayerMarker(player.playerMarker, opponent.playerMarker, board, index, emptySlots);             
+        })
     }
 }
 
-function addOpponentMarker(opponentMarker, playerMarker, board, index) {
-    const random = Math.floor(Math.random() * 9);
-    
+function randomNum(emptySlots) {
+    const rand = Math.floor(Math.random() * emptySlots.length);
+    return rand;
 }
 
-function addPlayerMarker(playerMarker, opponentMarker, board, index) {
+function addOpponentMarker(opponentMarker, board, emptySlots) {
+    const randNum = randomNum(emptySlots);
+    const opponentIndex = Number(emptySlots[randNum]);
+    board[opponentIndex] = opponentMarker;
+    emptySlots.splice(randNum, 1);
+}
+
+function addPlayerMarker(playerMarker, opponentMarker, board, index, emptySlots) {
     if(!board[index]){
-        console.log(playerMarker);
-        board[index] = playerMarker;
-
+        const boardIndex = index;
+        board[boardIndex] = playerMarker
+        emptySlots.splice(emptySlots.indexOf(boardIndex.toString()), 1);
+        addOpponentMarker(opponentMarker, board, emptySlots)
+        displayGameBoard(board);    
         } else if(board[index] == opponentMarker || board[index] == playerMarker) {
             console.log('try a new spot')
-            console.log(board);
     } 
-    console.log(board)
+}
+
+function displayGameBoard(board) {
+    for (i = 0; i < board.length; i++) {
+        let gridSlot = document.getElementById(i);
+        gridSlot.innerHTML = board[i]
+    }
 }
 
 gameboard();
